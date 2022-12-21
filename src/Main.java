bfd0e518e31611b5cdeef878e7a5a6ce850bd0c3
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2022-12-21 15:40:16 +0100

    fisrt commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        sayHello("Hatem");
+    }
+
+    public static void sayHello(String name){
+        System.out.println("Hello "  + name.toUpperCase());
+
+    }
+}
