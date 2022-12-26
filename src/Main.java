748b6e283e0b5874e85bf2049895c115fc7b4ead
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2022-12-26 01:59:36 +0100

    les exceptions

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,31 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        sayHello("Hatem");
-    }
+        FileReader fileReader = null;
+        StringBuffer donnees = new StringBuffer();
+        
+        try{
+            fileReader = new FileReader("file.txt");
+            System.out.println("File Opened Successfully.");
+            char[] buffer = new char[2048];
+            int len;
+            while ((len = fileReader.read(buffer)) > 0) {
+                donnees.append(buffer, 0, len);
+            }
+        } catch(FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if(fileReader != null ){
+                fileReader.close();
+            }
 
-    public static void sayHello(String name){
-        System.out.println("Hello "  + name.toUpperCase());
-
+        }
+        System.out.println(donnees.toString());
     }
 }
