f7f75284d444e6066b58767ba4f9236ef086f1b0
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2022-12-30 14:50:38 +0100

    l'interface Collection en Java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,40 @@
-import java.util.Iterator;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        GenericList<Integer> list = new GenericList<>();
+        Collection<Integer> list = new ArrayList<>();
         list.add(5);
         list.add(15);
+        list.add(15);
 
-        // 1st way to parcourir une liste
-        Iterator<Integer> iterator = list.iterator();
-        while (iterator.hasNext()){
-            System.out.println(iterator.next());
-        }
 
-        // 2nd way to parcourir une liste
+        // add all in collection
+        Collections.addAll(list, 1, 4, 5);
+        // size
+        System.out.println("La taille de la collection est :" + list.size());
+        // delete from collection
+        list.remove(4);
+        // clear a list (delete all element)
+        // list.clear();
+        // verify is an empty list
+        System.out.println("La liste est vide ? " + list.isEmpty());
+        // contains
+        var result = list.contains(3);
+        System.out.println("La liste contient 3 ? " + result);
+        // casting to Array of Object
+        Object[] object = list.toArray();
+        // casting to array of Integer
+        Integer[] integers = list.toArray(new Integer[5]); // 5 is the length
+
+        // add all in other collection
+        Collection<Integer> others = new ArrayList<>();
+        others.addAll(list);
+
+        // comparing collection
+        System.out.println(others == list); // false : based on memory address
+        System.out.println(others.equals(list)); // true : based on content
+
+        // show collection
         for (Integer item: list) {
             System.out.println(item);
         }
