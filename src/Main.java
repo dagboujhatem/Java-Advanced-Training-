923276d0544b1becde1f07e64919aa811f5f4aab
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2022-12-30 13:19:13 +0100

    les types multiples dans méthodes génériques en Java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         System.out.println(Utils.min(8,9));
         System.out.println(Utils.min(8.6D,9.0D));
         System.out.println(Utils.min(8.65F,3.02F));
+
+        // multiple type in the generic method
+        Utils.printKeyValue(4, "Hatem");
+        Utils.printKeyValue(2F, 5);
     }
 }
--- src/Utils.java
+++ src/Utils.java
@@ -2,4 +2,8 @@ public class Utils {
     public static <T extends Comparable<T>> T min(T first, T second){
         return (first.compareTo(second)<0)? first: second;
     }
+
+    public static <K,V> void printKeyValue(K key, V value){
+        System.out.println(key + " " + value);
+    }
 }
