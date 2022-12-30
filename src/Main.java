90a2382e54f154a42270e27dc6570b2b9dd275a0
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2022-12-30 17:59:57 +0100

    l'interface Queue en Java

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,42 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // The Comparator interface
-        List<BankAccount> list = new ArrayList<>();
-        list.add(new BankAccount(45,"holder1", "email1"));
-        list.add(new BankAccount(455, "holder2", "email2"));
-        list.add(new BankAccount(35, "holder3", "email13"));
-        System.out.println(list);
-        Collections.sort(list, new BalanceComparator()); // sort with balance
-        System.out.println(list);
-        Collections.sort(list, new EmailComparator()); // sort with email
-        System.out.println(list);
+        // The Queue interface
+        // You can use PriorityQueue ("PriorityQueue" => "File d'attente prioritaire")
+        // or you can use (i.e ArrayDeque "Array double ended queue" => "File d'attente à double extrémité")
+        Queue<Integer> queue = new PriorityQueue<>();
+        queue.add(3);
+        queue.add(2);
+        queue.add(1);
+        System.out.println(queue);
+
+        // add new item in the queue
+        queue.add(5);
+        System.out.println(queue);
+
+        // add in the queue (2nd way) but it return a status "true/false" of adding
+        // Raison : parfois la taillé des queue sont limité
+        // return true if the adding new item in the queue or false if is not possibly to add this item
+        boolean isAddedSuccessfully = queue.offer(6);
+        System.out.println(isAddedSuccessfully);
+        System.out.println(queue);
+
+        // get the first element in the queue or throw new exception "NoSuchElementException" if the queue is empty
+        Integer result = queue.element();
+        System.out.println(result);
+
+        // get the fist element in the queue or null if the queue is empty
+        Integer peek = queue.peek();
+        System.out.println(peek);
+
+        // remove the first element in the queue or throw new exception "NoSuchElementException" if the queue is empty
+        queue.remove();
+        System.out.println(queue);
+
+        // remove the first element in the queue or null if the queue is empty
+        queue.poll();
+        System.out.println(queue);
+
 
     }
 }
