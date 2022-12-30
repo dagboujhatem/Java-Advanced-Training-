e8e41bfdcc444d74a71813b5f824f31ae3dca9ee
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2022-12-30 14:30:22 +0100

    l'interface Iterable en Java

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,14 @@ public class Main {
         GenericList<Integer> list = new GenericList<>();
         list.add(5);
         list.add(15);
+
         // 1st way to parcourir une liste
         Iterator<Integer> iterator = list.iterator();
         while (iterator.hasNext()){
             System.out.println(iterator.next());
         }
 
-
-        // énd way to parcourir une liste
+        // 2nd way to parcourir une liste
         for (Integer item: list) {
             System.out.println(item);
         }
