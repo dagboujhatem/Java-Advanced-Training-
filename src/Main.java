b41e6c44de6ed16515916a192740ed3fe8191b60
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2022-12-30 11:13:27 +0100

    la géniricité des listes dans Java

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,16 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        List myList = new List();
-        myList.add(5);
-        int intItem = (int) myList.get(0);
-        System.out.println(intItem);
+        GenericList<String> myList = new GenericList<String>();
 
-        // add a string
         myList.add("hello");
-        String stringItem = (String) myList.get(1);
-        System.out.println(stringItem);
+        String itemOne = myList.get(0);
+        System.out.println(itemOne);
 
-        // add a bank account
-        myList.add(new BankAccount());
-        BankAccount bankAccountItem = (BankAccount) myList.get(2);
-        System.out.println(bankAccountItem);
+        // add a new string
+        myList.add("hello");
+        String itemTwo = myList.get(1);
+        System.out.println(itemTwo);
+
     }
 }
